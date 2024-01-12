b1cb4d4c5e54303e3a19d8814a0f0c1cf946247f
Francesco Renis <frarenis00@gmail.com>
2024-01-12 13:50:27 +0100

    Graph generator tutto funzionante tutto integrato (prima non lo era)

--- GraphGenerator/graph_generator.cpp
+++ GraphGenerator/graph_generator.cpp
@@ -7,15 +7,12 @@
 #include <fstream>
 #include <vector>
 #include <random>
-#include <algorithm>
 #include <chrono>
 #include <ctime>
 #include <iomanip>
 #include <sstream>
-#include <tuple>
-#include <map>
 
-//#define DEBUG
+#define DEBUG
 #define MAX_EDGE_WEIGHT 10
 
 using namespace std;
@@ -23,118 +20,103 @@ using namespace std;
 int main(int argc, char** argv) {
     srand(time(NULL));
 
-    //#ifndef DEBUG
-
+    #ifndef DEBUG
     if (argc != 3) {
         cout << "Usage: ./graph_generator <num_nodes> <num_edges>" << endl;
         return 1;
     }
     int num_nodes = std::stoi(argv[1]);
     int num_edges = std::stoi(argv[2]);
-
-    /*#endif
+    #endif
     // DEBUG
     #ifdef DEBUG
     int num_nodes = 5;
     int num_edges = 6;
-    #endif*/
+    #endif
 
-    // Generate Nodes
+    auto now = chrono::system_clock::now();
+    auto now_ms = chrono::time_point_cast<chrono::milliseconds>(now);
+    auto now_c = now_ms.time_since_epoch().count();
+
+    // Generate nodes with random weights
     vector<pair<int, int>> nodes(num_nodes);
-
     for (int i = 0; i < num_nodes; i++) {
         nodes[i] = { i, rand() % 10 + 1 };
     }
 
-    // Generate Edges, Standard version
-    map<pair<int, int>, int> edges;
-
-    /*while(edges.size() < num_edges) {
-        int u = (rand()*rand()) % num_nodes;
-        int v = (rand()*rand()) % num_nodes;
-
-        edges[{u, v}] = rand() % 10 + 1 ;
-        cout <<"Arco: " << edges.size() << endl;
-    }*/
-
-
-    // Generate Edges, Adjacency matrix
+    // Generate edges with random weights
     int madj[num_nodes][num_nodes] = {};
-
     for (int i = 0; i < num_edges; i++) {
-        int u = (rand()*rand()) % num_nodes;
-        int v = (rand()*rand()) % num_nodes;
+        int u = rand() % num_nodes;
+        int v = rand() % num_nodes;
         while (u == v || madj[u][v] != 0) {
-            u = (rand()*rand()) % num_nodes;
-            v = (rand()*rand()) % num_nodes;
+            u = rand() % num_nodes;
+            v = rand() % num_nodes;
         }
         int w = rand() % MAX_EDGE_WEIGHT + 1;
         madj[u][v] = w;
         madj[v][u] = w;
     }
 
+    // Write num nodes and edges to files
+    ostringstream filename, filename_bin, filename_metis;
+    string path = "../data/graph_" + to_string(num_nodes) + "_" + to_string(num_edges) + "/";
+    filename << path;
+    filename_bin << path;
+    filename_metis << path;
+    ofstream outfile, outfile_bin, outfile_metis;
 
-    // Write Nodes and Edges to file, for METIS and our GA
-    auto now = chrono::system_clock::now();
-    auto now_ms = chrono::time_point_cast<chrono::milliseconds>(now);
-    auto now_c = now_ms.time_since_epoch().count();
-
-    ostringstream filename, filename_metis;
-    filename << "graph_" << num_nodes << "_" << num_edges << ".txt";
-    filename_metis << "graph_" << num_nodes << "_" << num_edges << "_metis.txt";
-    ofstream outfile("../data/" + filename.str());
-    ofstream outfile_metis("../data/METIS/" + filename_metis.str());
+    filename << "standard_text.txt";
+    outfile.open(filename.str());
     outfile << num_nodes << endl;
     outfile << num_edges << endl;
-    outfile_metis << num_nodes << " " << num_edges << " 011" << endl;
-    /* from LSB(right) to MSB(left)
-     * 0: nodes have no 'size' (1 node has 1 weight value, no 'multi-constrained partition')
-     * 1: nodes have specified weight
-     * 1: edges have specified weight
-     *
-     * source: METIS manual
-     */
 
-    // Default Nodes Output
+    filename_bin << "standard_binary.bin";
+    outfile_bin.open(filename.str(), ios::binary);
+    outfile_bin.write((char*)(&num_nodes),sizeof(int));
+    outfile_bin.write((char*)(&num_edges),sizeof(int));
+
+    filename_metis << "metis_text.txt";
+    outfile_metis.open(filename_metis.str());
+    outfile_metis << num_nodes << " " << num_edges << " 011" << endl;
+
+
+    // default nodes format
     for (auto node : nodes) {
         outfile << node.first << " " << node.second << endl;
+        outfile_bin.write((char*)(&node.first), sizeof(int));
+        outfile_bin.write((char*)(&node.second), sizeof(int));
     }
-
-    /* Default Nodes Output
-    for (auto edge : edges) {
-        outfile << edge.first.first << " " << edge.first.second << " " << edge.second << endl;
-    }
-     */
-
     // default edges format + metis format (nodes go from 1 to num_nodes, instead of 0 to num_nodes-1)
     for(int i=0; i<num_nodes;i++) {
         outfile_metis << nodes.at(i).second;
-        for (int j = 0; j < num_nodes; j++) {
-            if (j <= i) {// full line, prints same edge twice (undirected graph, matrix in simmetric), needed for metis format
+        for (int j=0; j<num_nodes; j++) {
+            if (j<=i) {// full line, prints same edge twice (undirected graph, matrix in simmetric), needed for metis format
                 if (madj[i][j] != 0)
-                    outfile_metis << " " << j + 1 << " " << madj[i][j];
-            } else// j > i, upper diagonal only, prints edges only once
-            if (madj[i][j] != 0) {
-                outfile_metis << " " << j + 1 << " " << madj[i][j];
-                outfile << i << " " << j << " " << madj[i][j] << endl;
+                    outfile_metis << " " << j+1 << " " << madj[i][j];
             }
+            else// j > i, upper diagonal only, prints edges only once
+                if (madj[i][j] != 0) {
+                    outfile_metis << " " << j+1 << " " << madj[i][j];
+                    outfile << i << " " << j << " " << madj[i][j] << endl;
+                    outfile_bin.write((char*)(&i), sizeof(int));
+                    outfile_bin.write((char*)(&j), sizeof(int));
+                    outfile_bin.write((char*)(&madj[i][j]), sizeof(int));
+                }
         }
         outfile_metis << endl;
     }
-
-    outfile_metis.close();
     outfile.close();
+    outfile_bin.close();
+    outfile_metis.close();
 
     auto now_now = chrono::system_clock::now();
     auto now_now_ms = chrono::time_point_cast<chrono::milliseconds>(now_now);
     auto now_now_c = now_now_ms.time_since_epoch().count();
 
-    std::cout << "Graph file generated and saved to file: ./data/" << filename.str() << std::endl;
-    std::cout << "Graph file(metis format) generated and saved to file: ./data/METIS/" << filename_metis.str() << std::endl;
-
-    std::cout << now_c << std::endl;
-    std::cout << now_now_c << std::endl;
-    std::cout << now_now_c - now_c << std::endl;
+    cout << "Graph files generated and saved to path: " << path << endl;
+    cout << now_now_c - now_c << " ms" << endl;
+    cout << (now_now_c - now_c)/1000 << setprecision(5) << " seconds" << endl;
 
     return 0;
 }
