36b419b6c4aa976bc48472aef662e912023902c3
gregorio-nic <gregorio.nicora@gmail.com>
2024-01-13 20:15:20 +0100

    main_test_parallelization probabilmente risolto :)

--- main_test_parallelization.cpp
+++ main_test_parallelization.cpp
@@ -6,39 +6,41 @@
 #include <iostream>
 #include <thread>
 #include <barrier>
+#include <mutex>
 
 using namespace std;
 
 struct GA_parameters{
     int NUM_PARTITIONS = 0;
-    int NUM_OFFSPRING = 15;
-    int NUM_GENERATIONS = 30;
-    int POPULATION_SIZE = 20;
+    int NUM_OFFSPRING = 4;
+    int NUM_GENERATIONS = 4;
+    int POPULATION_SIZE = 4;
     int NUM_ISLANDS = 4;
     int NUM_ERAS = 5;
     int NUM_MIGRANTS = 2;
 } GA_parameters;
 
+mutex printMutex;
+
 
 void Eras_parallel(int thread_id, barrier<>& b1, barrier<>& b2) {
-
+    printMutex.lock();
+    cout << " THREAD " << thread_id  << endl;
+    printMutex.unlock();
     for(int e = 0; e<GA_parameters.NUM_ERAS; e++){
 
         for (int g = 0; g < GA_parameters.NUM_GENERATIONS; g++) {
-            cout << "Starting Generation n_" << g << endl;
-
-            for (int i = 0; i < GA_parameters.NUM_OFFSPRING; i++) {
-
-                cout << " THREAD " << thread_id  << " era " << e << " generation " << g << " offspring " << i << endl;
-
-            }
+            //cout << " THREAD " << thread_id  << " Starting Generation n_" << g << endl;
 
         }
 
-        b2.arrive_and_wait();
-        // WAIT ...
         b1.arrive_and_wait();
+        printMutex.lock();
+        cout << " THREAD " << thread_id  << " arrivato a barrier b2 in era " << e << endl;
+        printMutex.unlock();
+        // WAIT ...
 
+        b2.arrive_and_wait();
     }
 }
 
@@ -46,24 +48,23 @@ void Eras_parallel(int thread_id, barrier<>& b1, barrier<>& b2) {
 
 void Galapagos_parallel(){
 
-    vector<thread> Islands;
+    vector<std::thread> Islands;
 
-    barrier barrier_1_cpp(GA_parameters.NUM_ISLANDS);
-    barrier barrier_2_cpp(GA_parameters.NUM_ISLANDS);
+    barrier<> barrier_1_cpp(GA_parameters.NUM_ISLANDS + 1 );
+    barrier<> barrier_2_cpp(GA_parameters.NUM_ISLANDS + 1 );
 
 
-    for(int i = 0; i<GA_parameters.NUM_ISLANDS; i++) {
-        cout << "Starting Isola n_" << i << endl;
-        Islands.emplace_back( Eras_parallel, i, barrier_1_cpp, barrier_2_cpp);
-
+    for(int i = 0; i<GA_parameters.NUM_ISLANDS; ++i) {
+        //cout << "Starting Isola n_" << i << endl;
+        Islands.emplace_back( [=, &barrier_1_cpp, &barrier_2_cpp] {Eras_parallel(i, barrier_1_cpp, barrier_2_cpp);});
     }
 
-    for(int e = 1; e<GA_parameters.NUM_ERAS; e++){
+    for(int e = 0; e<GA_parameters.NUM_ERAS; e++){
 
         barrier_1_cpp.arrive_and_wait();
-
-        cout << "Sono il master!! \n" << endl;
-
+        printMutex.lock();
+        cout << "Sono il master!! GIRO: " << e << endl;
+        printMutex.unlock();
         barrier_2_cpp.arrive_and_wait();
 
     }
