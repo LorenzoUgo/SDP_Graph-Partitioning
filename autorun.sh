a6bf206f4e5da6617ddeaac4ef7a68c0333c55f3
Francesco Renis <frarenis00@gmail.com>
2024-02-09 19:44:08 +0100

    modified autorun

--- autorun.sh
+++ autorun.sh
@@ -1,43 +1,39 @@
 #!/bin/sh
 
+if [ $# -eq 0 ]
+then
+	echo "Usage: $0 <data/graph_#nodes_#edges/>"
+	return
+else
+	filename=$1
+	numpart=$2
+fi
+
+gfolder=$1
 nthread="2 4 6 8"
 npart="2 4 6 8"
-
-# AAAAAAA test del cazzo giusto vedere se funziona prima di bruciarti la cpu 
-# parallel
-#for nt in $nthread; do
-#	for np in $npart; do
-#		./graph_partitioner data/graph_750000_2250000/ --population 2 --new 1 --mig 1 --gen 1 --parallel --bal --isl "$nt" --part "$np"
-#	done
-#done
-
-# sequential
-#for np in $npart; do
-#	./graph_partitioner data/graph_750000_2250000/ --bal --population 2 --new 1 --mig 1 --gen 1 --part "$np"
-#done
 
 # parallel
 for nt in $nthread; do
 	for np in $npart; do
-		./graph_partitioner data/graph_750000_2250000/ --parallel --bal --isl "$nt" --part "$np"
+		./graph_partitioner "$gfolder" --parallel --bal --isl "$nt" --part "$np"
+		# Test ./graph_partitioner "$gfolder" --parallel --bal --isl "$nt" --part "$np" --population 1 --new 1 --mig 1 --gen 1 --era 1
 	done
 done
 
 # sequential
 for np in $npart; do
-	./graph_partitioner data/graph_750000_2250000/ --bal --population 60 --new 30 --gen 1000 --part "$np"
+	./graph_partitioner "$gfolder" --bal --population 60 --new 30 --gen 600 --part "$np"
+	# Test./graph_partitioner "$gfolder" --bal --isl "$nt" --part "$np" --population 1 --new 1 --mig 1 --gen 1 --era 1
 done
 
-mv data/graph_750000_2250000/gapart.balanced.parallel.2.* data/graph_750000_2250000/results/2part/
-mv data/graph_750000_2250000/gapart.balanced.2.txt data/graph_750000_2250000/results/2part/
-
-mv data/graph_750000_2250000/gapart.balanced.parallel.4.* data/graph_750000_2250000/results/4part/
-mv data/graph_750000_2250000/gapart.balanced.4.txt data/graph_750000_2250000/results/4part/
-
-mv data/graph_750000_2250000/gapart.balanced.parallel.6.* data/graph_750000_2250000/results/6part/
-mv data/graph_750000_2250000/gapart.balanced.6.txt data/graph_750000_2250000/results/6part/
-
-mv data/graph_750000_2250000/gapart.balanced.parallel.8.* data/graph_750000_2250000/results/8part/
-mv data/graph_750000_2250000/gapart.balanced.8.txt data/graph_750000_2250000/results/8part/
+# create resulsts folder and move files
+for np in $npart; do
+	mkdir -p "$gfolder"results/"$np"part/
+done
+for np in $npart; do
+	mv "$gfolder"gapart.balanced.parallel."$np".* "$gfolder"results/"$np"part/
+	mv "$gfolder"gapart.balanced."$np".txt "$gfolder"results/"$np"part/
+done
 
 echo "\nDone!\n"
